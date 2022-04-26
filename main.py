6bb4c2c1aa410acc1513024a8d1d184f92721dce
stre1ok <strelok-1111@yandex.ru>
2022-04-26 14:25:58 +0300

    try2

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("5112916900:AAH3sDGJ1LYO0LmBNRM1BPBaXHbRkLYqWmI")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
