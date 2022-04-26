9fc862e4161423f3c98fd4cfa3531cc9429c21c3
stre1ok <strelok-1111@yandex.ru>
2022-04-26 15:35:19 +0300

    Update main.py
    try 6 from git

--- main.py
+++ main.py
@@ -39,4 +39,25 @@ def age_registration(message):
         except Exception:
             bot.send_message(message.from_user.id, 'Only integer numbers!')
 
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    keyboard.add(key_yes)
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+    question = 'Тебе ' + str(age) + ' лет? И тебя зовут: ' + name + ' ' + surname + '?'
+    bot.send_message(message.from_user.id, text = question, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, "Приятно познакомиться! Теперь запишу в БД!")
+    elif call.data == "no":
+        elif call.data == "no":
+        name = ''
+        surname = ''
+        age = 0
+        bot.send_message(call.message.chat.id, "Let's try again!")
+        bot.send_message(call.message.chat.id, "Write your name")
+        bot.register_next_step_handler(call.message, name_registration)
+
 bot.polling()
